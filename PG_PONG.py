4da9a0930d2e7c60de4da34b29803288a5ec59b5
Neuroosi <Neuroosi@users.noreply.github.com>
2021-12-29 13:12:17 +0200

    Add files via upload

--- PG_PONG.py
+++ PG_PONG.py
@@ -9,8 +9,6 @@ from ple.games.pong import Pong
 from ple import PLE
 ##Hyperparameters
 learning_rate = 0.0001
-REPLAY_MEMORY_SIZE=100000
-BATCH_SIZE = 64
 GAMMA = 0.99
 EPISODES = 5000
 
@@ -25,7 +23,7 @@ class PLNET:
         self.actions = []
     def create_model(self):
         model = Sequential()
-        model.add(Dense(256,input_shape = (self.obsSpaceSize,) ))
+        model.add(Dense(256,input_shape = (self.obsSpaceSize,), activation = 'relu' ))
         model.add(Dense(256, activation = 'relu'))
         ##model.add(Dense(64, activation = 'relu'))
         model.add(Dense(self.actionSpaceSize, activation = 'softmax'))
@@ -39,20 +37,26 @@ class PLNET:
         cache[action] = 1
         self.actions.append(cache)
 
-    def train(self):
-        G = []
+    def discounted_reward(self):
+        G = np.zeros(len(self.rewards))
         ##Calculate discounted reward
-        for t in range(len(self.states)):
-            v_t = [reward*GAMMA**(k+t-t) for k, reward in enumerate(self.rewards[t:len(self.rewards)])]
-            G.append(np.sum(v_t))
+        cache = 0
+        for t in reversed(range(0, len(self.rewards))):
+            if self.rewards[t] != 0: cache = 0
+            cache = cache*GAMMA + self.rewards[t]
+            G[t] = cache
         ##Normalize
-        G = np.array(G)
-        G = (G-np.mean(G))/np.std(G)
+        G = (G-np.mean(G))/(np.std(G))
+        return G
+    def train(self, G):
         ##Train the model
-        self.model.fit(np.array(self.states), np.array(self.actions), sample_weight= G, epochs = 1, verbose = 1)
+        #self.model.fit(np.array(self.states), np.array(self.actions), epochs = 1, verbose = 1)
+        his = self.model.train_on_batch(np.array(self.states), np.array(self.actions), sample_weight=G)
+        print(his)
         self.actions, self.states, self.rewards = [],[],[]        
     def saveModel(self):
         self.model.save("/home/joel/PONG-PG/bestmodel")
+        print("Model saved!")
     def loadModel(self):
         self.model = keras.models.load_model("/home/joel/PONG-PG/bestmodel/")
         
@@ -63,17 +67,23 @@ class PLNET:
         return np.random.choice(self.actionSpaceSize, p = probs)
 
 if __name__ == "__main__":
-    game = Pong(500,500)
-    p = PLE(game, fps = 30, display_screen=False)
+    #game = FlappyBird(width=288, height=512, pipe_gap=100)
+    game = Pong(500,500, MAX_SCORE = 2)
+    p = PLE(game, fps = 30, frame_skip = 3, display_screen=True)
     p.init()
     state = []
     agent = PLNET(len(p.getActionSet()), len(p.getGameState()))
-    t = 0
-    for episode in range(EPISODES+500000000000):
+    avg_reward = 0
+    total_time = 0
+    max_t = -10000000
+    max_reward = -10000000
+    for episode in range(1,EPISODES+500000000000):
         p.reset_game()
         cumureward = 0
         state = np.array(list(p.getGameState().values()))
+        t = 0
         while True:
+            state = (state - np.mean(state))/np.std(state)
             action = agent.getPrediction(state)
             reward = p.act(p.getActionSet()[action])
             next_state = np.array(list(p.getGameState().values()))
@@ -81,9 +91,14 @@ if __name__ == "__main__":
             state = next_state
             t+=1
             cumureward += reward
+            avg_reward += reward
             if t % 10000 == 0:
                 agent.saveModel()
             if p.game_over() == True:
                 break
-        print("Score: ",cumureward,"Time:", t, " Episode:", episode)
-        agent.train()
+        estimate_value_function = agent.discounted_reward()
+        max_t = max(max_t, t)
+        total_time += t
+        max_reward = max(cumureward, max_reward)
+        print("Score: ",cumureward, " Avg score: ",avg_reward/episode, " Max score: " ,max_reward,"Time:", t," Max time: ", max_t, "Avg time: " ,total_time/episode, "Discounted reward max:", np.amax(estimate_value_function), " Min discounted reward: ", np.amin(estimate_value_function) ,"Discounted reward avg: ", np.mean(estimate_value_function) ," Episode:", episode)
+        agent.train(estimate_value_function)
